880d5a82713dde3900ffd4b799e4fd349b3e5dee
lgclgclgc111 <lgclgclgc111@126.com>
2017-03-15 22:21:18 +0800

    111

---
+++ src/main/java/com/sectong/entity/IsAlreadyHaveClassroomResult.java
@@ -0,0 +1,28 @@
+package com.sectong.entity;
+
+public class IsAlreadyHaveClassroomResult
+{
+	private Integer CurriculumId;
+	private String QrcodeInfo;
+
+	public Integer getCurriculumId()
+	{
+		return CurriculumId;
+	}
+
+	public void setCurriculumId(Integer curriculumId)
+	{
+		CurriculumId = curriculumId;
+	}
+
+	public String getQrcodeInfo()
+	{
+		return QrcodeInfo;
+	}
+
+	public void setQrcodeInfo(String qrcodeInfo)
+	{
+		QrcodeInfo = qrcodeInfo;
+	}
+
+}
